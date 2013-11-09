cc8a4a9ba32528b58dbe6418e12493d9e524b02f
Yasuharu Nakano <ynak@jggug.org>
2013-11-09 13:36:54 +0900

    ... P.21

---
+++ WebSocket-HoL/src/java/jp/co/oracle/cdis/IndexPageMgdBean.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package jp.co.oracle.cdis;
+
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+
+/**
+ *
+ * @author ynak
+ */
+@Named(value = "indexPageMgdBean")
+@RequestScoped
+public class IndexPageMgdBean {
+
+    /**
+     * Creates a new instance of IndexPageMgdBean
+     */
+    public IndexPageMgdBean() {
+    }
+    
+}
