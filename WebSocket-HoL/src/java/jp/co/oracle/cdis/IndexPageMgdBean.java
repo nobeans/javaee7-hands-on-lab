50b076ef0687d47cb2da192e3a5bc2c385a5ddfb
Yasuharu Nakano <ynak@jggug.org>
2013-11-09 14:04:54 +0900

    ... P.49

--- WebSocket-HoL/src/java/jp/co/oracle/cdis/IndexPageMgdBean.java
+++ WebSocket-HoL/src/java/jp/co/oracle/cdis/IndexPageMgdBean.java
@@ -6,8 +6,13 @@
 
 package jp.co.oracle.cdis;
 
+import javax.annotation.Resource;
 import javax.inject.Named;
 import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.jms.JMSConnectionFactory;
+import javax.jms.JMSContext;
+import javax.jms.Topic;
 
 /**
  *
@@ -17,6 +22,13 @@ import javax.enterprise.context.RequestScoped;
 @RequestScoped
 public class IndexPageMgdBean {
 
+    @Inject
+    @JMSConnectionFactory("jms/topicCon")
+    JMSContext context;
+    
+    @Resource(mappedName = "jms/inforegtopic")
+    Topic topic;
+    
     private String message;
     
     /**
@@ -41,6 +53,7 @@ public class IndexPageMgdBean {
  
     public String pushSendButton() {
         System.out.println(getMessage());
+        context.createProducer().send(topic, getMessage());
         return "";
     }
 }
